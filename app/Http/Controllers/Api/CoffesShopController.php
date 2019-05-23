257753725b04d8c3e7fb660cfe5e8f31bc251071
Raphael Morales <raphael.morales@outlook.com>
2019-05-23 15:20:17 -0300

    Api Route

--- app/Http/Controllers/Api/CoffesShopController.php
+++ app/Http/Controllers/Api/CoffesShopController.php
@@ -10,13 +10,13 @@ use Validator;
 use Hash;
 
 // Models
-use App\Models\CoffesShop;
+use App\Models\CoffeesShop;
 
 class CoffesShopController extends Controller
 {
   public function get()
   {
-    $coffeesShop = CoffesShop::orderBy('order')->get();
+    $coffeesShop = CoffeesShop::orderBy('order')->get();
     if(count($coffeesShop) == 0) {return Response::json(array( 'error' => true, 'msg' => 'Not Found Data'),404); }
     return Response::json(array( 'error' => false, 'msg' => 'Lista', 'data' => $coffeesShop),200);
   }
