e58c64adc3f2d2485ff225a19666635ff3a54285
Raphael Morales <raphael.morales@outlook.com>
2019-05-23 15:21:09 -0300

    Api Route

--- app/Http/Controllers/Api/CoffesShopController.php
+++ app/Http/Controllers/Api/CoffesShopController.php
@@ -16,7 +16,7 @@ class CoffesShopController extends Controller
 {
   public function get()
   {
-    $coffeesShop = CoffeesShop::orderBy('order')->get();
+    $coffeesShop = CoffeesShop::orderBy('id')->get();
     if(count($coffeesShop) == 0) {return Response::json(array( 'error' => true, 'msg' => 'Not Found Data'),404); }
     return Response::json(array( 'error' => false, 'msg' => 'Lista', 'data' => $coffeesShop),200);
   }
