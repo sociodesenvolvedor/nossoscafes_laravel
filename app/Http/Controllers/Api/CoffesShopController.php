213b4e067f6097a36c72d01f0c2b51f2823fb97d
Raphael Morales <raphael.morales@outlook.com>
2019-05-23 15:49:32 -0300

    Api Route

--- app/Http/Controllers/Api/CoffesShopController.php
+++ app/Http/Controllers/Api/CoffesShopController.php
@@ -27,6 +27,7 @@ class CoffesShopController extends Controller
       $new =
       [
         "id" =>  $c->id,
+        "idFake" =>  $c->id,
         "user_id" => $c->user_id,
         "logo" => $c->logo,
         "description" => $c->description,
